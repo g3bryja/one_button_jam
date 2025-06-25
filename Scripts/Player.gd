29e843019b5698d3819d8970b5eaa5ab07f9d67c
Gino Bryja <g3bryja@gmail.com>
2025-06-25 18:25:25 -0400

    Added goose sprites

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -8,6 +8,18 @@ var _DOWN = 'down'
 
 var direction = _RIGHT
 
+@export var sprite_up: Sprite2D
+@export var sprite_down: Sprite2D
+@export var sprite_left: Sprite2D
+@export var sprite_right: Sprite2D
+
+
+func _ready():
+	sprite_right.visible = false
+	sprite_down.visible = false
+	sprite_left.visible = false
+	sprite_up.visible = false
+
 
 func _process(_delta):
 	var dir = int(rotation_degrees / 90) % 4
@@ -20,8 +32,14 @@ func _process(_delta):
 			direction = _LEFT
 		3:
 			direction = _UP
-	print(dir)
+	_update_sprite()
 
 
-func _on_star_collision(collision: Area2D):
+func _update_sprite():
+	sprite_right.visible = direction == _RIGHT
+	sprite_down.visible = direction == _DOWN
+	sprite_left.visible = direction == _LEFT
+	sprite_up.visible = direction == _UP
+
+func _on_star_collision(_collision: Area2D):
 	print("WIN")
--- Scripts/Rotate CW Action.gd
+++ Scripts/Rotate CW Action.gd
@@ -3,3 +3,4 @@ extends "res://Scripts/Template Action.gd"
 
 func _on_action():
 	player.rotate(-PI/2)
+	player.rotation = fmod(player.rotation, 2 * PI)

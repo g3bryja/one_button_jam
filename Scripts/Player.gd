f35246eb656a5100075f984859740fb941077446
Gino Bryja <g3bryja@gmail.com>
2025-06-24 22:58:45 -0400

    Added player direction

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,5 +1,27 @@
 extends Node2D
 
 
+var _LEFT = 'left'
+var _RIGHT = 'right'
+var _UP = 'up'
+var _DOWN = 'down'
+
+var direction = _RIGHT
+
+
+func _process(_delta):
+	var dir = int(rotation_degrees / 90) % 4
+	match dir:
+		0:
+			direction = _RIGHT
+		1:
+			direction = _DOWN
+		2:
+			direction = _LEFT
+		3:
+			direction = _UP
+	print(dir)
+
+
 func _on_star_collision(collision: Area2D):
 	print("WIN")
